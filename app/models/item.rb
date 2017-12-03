21e673c0a5ac72172fa6d837b17d7a1e379eae09
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-12-03 14:30:35 -0600

    dollar string

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,14 @@ class Item < ApplicationRecord
 
   has_many :items_wishlists, dependent: :destroy
 
+  include ActionView::Helpers::NumberHelper
+
+  def price_display
+    if price.blank? || price.zero?
+      return ''
+    end
+    precision = (price == price.round) ? 0 : 2  # (if no cents, don't show .00)
+    number_to_currency(price, precision: precision)
+  end
+
 end
